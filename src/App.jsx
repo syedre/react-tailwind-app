2aa8547084c024e65d768586c987c469809482d3
syedre <syed.rehan123@gmail.com>
2025-11-07 19:07:20 +0530

    tried suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Suspense, use, useState } from "react";
 import "./App.css";
 
 function App() {
@@ -9,13 +9,27 @@ function App() {
         <div className="bg-amber-50">2</div>
         <div className="bg-amber-50">3</div>
         <div className="bg-amber-50">4</div>
-        <div className="bg-amber-50 row-span-3">5</div>
+        <div className="bg-amber-50 row-span-3">
+          <Suspense fallback={<div>loading .......</div>}>
+            <Slow />
+          </Suspense>
+        </div>
         <div className="bg-amber-50 ">6</div>
         <div className="bg-amber-50  row-start-4">7</div>
         <div className="bg-amber-50 col-start-3 row-start-4">8</div>
       </div>
     </>
   );
+}
+
+async function getData() {
+  await new Promise((resolve) => setTimeout(resolve("yes"), 4000));
+  return "yeah";
+}
+
+function Slow() {
+  const data = use(getData());
+  return <div>{JSON.stringify(data)}</div>;
 }
 
 export default App;
