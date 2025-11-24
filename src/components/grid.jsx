67602049c93fc777731e2da3c426a2f3772123a1
syedre <syed.rehan123@gmail.com>
2025-11-24 19:54:34 +0530

    added search

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from "react";
 
+const _users = [
+  { id: 1, name: "John" },
+  { id: 2, name: "Emma" },
+  { id: 3, name: "Michael" },
+  { id: 4, name: "Sophia" },
+  { id: 5, name: "David" },
+  { id: 6, name: "Olivia" },
+  { id: 7, name: "Daniel" },
+  { id: 8, name: "Ava" },
+  { id: 9, name: "James" },
+  { id: 10, name: "Mia" },
+];
+
 const GridColor = () => {
   const [arr, setArr] = useState([
     { id: 3, color: false },
@@ -9,7 +22,12 @@ const GridColor = () => {
     { id: 55, color: false },
     { id: 22, color: false },
   ]);
+
+  const [users, setUsers] = useState(_users);
+  const [search, setSearch] = useState("");
+
   const [rev, setRev] = useState([]);
+  const [loading, setLoading] = useState(false);
   const styles = {
     container: {
       display: "grid",
@@ -22,8 +40,11 @@ const GridColor = () => {
 
   const handleClick = (id) => {
     const newArr = arr?.map((i) => {
+      const doesExist = rev?.find((i) => i?.id === id);
       if (i?.id === id) {
-        rev.push(i);
+        if (!!doesExist === false) {
+          rev.push(i);
+        }
         return { ...i, color: !i?.color };
       }
       return i;
@@ -31,30 +52,56 @@ const GridColor = () => {
     setArr(newArr);
   };
 
-  const handleSet = () => {
-    rev.reverse().forEach((item, index) => {
-      setTimeout(() => {
-        setArr((prevArr) =>
-          prevArr?.map((ele) =>
-            ele?.id === item?.id ? { ...ele, color: false } : ele
-          )
-        );
-      }, index * 1000);
+  const handleSet = async () => {
+    setLoading(true);
+    rev.reverse().forEach(async (item, index) => {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          setArr((prevArr) =>
+            prevArr?.map((ele) =>
+              ele?.id === item?.id ? { ...ele, color: false } : ele
+            )
+          );
+          resolve();
+        }, index * 1000);
+      });
+      pro.then(() => {
+        setLoading(false);
+      });
     });
   };
 
+  useEffect(() => {
+    if (rev.length === arr.length && loading === false) {
+      handleSet();
+    }
+  }, [JSON.stringify(rev)]);
+
   return (
-    <div style={styles.container}>
-      {arr?.map((i) => (
-        <div
-          className={i?.color === true ? "bg-green-400" : "bg-red-400"}
-          onClick={() => handleClick(i?.id)}
-        >
-          {i?.id}
-        </div>
-      ))}
-      <button onClick={handleSet}>reset</button>
-    </div>
+    <>
+      <div style={styles.container}>
+        {arr?.map((i) => (
+          <div
+            className={i?.color === true ? "bg-green-400" : "bg-red-400"}
+            onClick={() => handleClick(i?.id)}
+          >
+            {i?.id}
+          </div>
+        ))}
+        <button onClick={handleSet}>reset</button>
+      </div>
+      {users
+        ?.filter((ele) => {
+          return (
+            // search.toLowerCase() === "" ||
+            ele?.name.toLowerCase().includes(search.toLowerCase())
+          );
+        })
+        .map((i) => {
+          return <div>{i?.name}</div>;
+        })}
+      <input value={search} onChange={(e) => setSearch(e.target.value)} />
+    </>
   );
 };
 
