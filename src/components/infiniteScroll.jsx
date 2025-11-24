2a2d2d0f93152bdcb5148df28fa0f36d0c731095
syedre <syed.rehan123@gmail.com>
2025-11-24 05:47:22 +0530

    added component

--- src/components/infiniteScroll.jsx
+++ src/components/infiniteScroll.jsx
@@ -1,5 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 
+const Child = ({ data }) => {
+  const [showPhone, setShowPhone] = useState(false);
+  return (
+    <div>
+      <button className={"h-8"} onClick={() => setShowPhone(!showPhone)}>
+        {data?.firstName}
+      </button>
+      {showPhone === true && data?.age}
+    </div>
+  );
+};
+
 const InfiniteScroll = () => {
   const [data, setData] = useState([]);
   const [skip, setSkip] = useState(0);
@@ -20,7 +32,7 @@ const InfiniteScroll = () => {
       `https://dummyjson.com/users?limit=${limit}&skip=${skip}&select=firstName,age `
     );
     const result = await response.json();
-    setData((prev) => [...prev, ...result.users]);
+    setData([...data, ...result.users]);
     setIsLoading(false);
   }
 
@@ -38,15 +50,17 @@ const InfiniteScroll = () => {
   }, [skip]);
 
   return (
-    <div
-      className="h-[300px] border border-stone-200 "
-      style={styles.boxq}
-      onScroll={handlescroll}
-      ref={divRef}
-    >
-      <div>Infinite scroll</div>
-      <div>
-        {data && data?.map((i) => <div className={"h-8"}>{i?.firstName}</div>)}
+    <div className="h-[300px] border border-stone-300 flex flex-col ">
+      <div className="h-15 bg-stone-100 sticky top-0 left-0 flex items-center">
+        Infinite scroll
+      </div>
+      <div
+        className="flex-1"
+        onScroll={handlescroll}
+        ref={divRef}
+        style={styles.boxq}
+      >
+        {data && data?.map((i) => <Child data={i} />)}
       </div>
       {isLoading === true && <div>loading....</div>}
     </div>
